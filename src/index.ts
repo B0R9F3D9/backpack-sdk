34c4da291de2cd0e192089df5d1b74212f198a87
Уэйд Уилсон <usasupsn@gmail.com>
2025-03-12 00:53:12 +0100

    add types to export

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
 export { AuthClient } from './client/auth';
 export { PublicClient } from './client/public';
+export type {
+	Token,
+	Balances,
+	BorrowLendPosition,
+	OpenedLimitOrder,
+	OpenedMarketOrder,
+	Depth,
+	Reward,
+} from './types';
